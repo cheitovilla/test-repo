6b6204a7ebfad23e00c8637d3d97358c0c786ffa
Jaime Garcia <che1t0v1ll4@outlook.com>
2024-11-06 17:08:51 -0500

    Revert "script walk finished"
    This reverts commit 0cb0577195d27f85e999b44142a23f32c7d2f90f.

--- Assets/Scripts/Walk.cs
+++ Assets/Scripts/Walk.cs
@@ -7,7 +7,7 @@ public class Walk : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("walking");
+        
     }
 
     // Update is called once per frame
